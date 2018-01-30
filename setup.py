00ed6d7428b2be7dd8ba8ce95cdf2d2b364c2979
Jason Carver <ut96caarrs@snkmail.com>
2018-01-30 11:09:18 -0800

    Bump version: 0.1.0-alpha.1 → 0.1.0-alpha.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ extras_require['dev'] = (
 setup(
     name='hexbytes',
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version='0.1.0-alpha.1',
+    version='0.1.0-alpha.2',
     description="""hexbytes: Python `bytes` subclass that decodes hex, with a readable console output""",
     long_description_markdown_filename='README.md',
     author='Jason Carver',
