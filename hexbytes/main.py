905b3b85b8011951a0f15a62b6fcdfec74cb0ae1
Jason Carver <ut96caarrs@snkmail.com>
2019-06-03 12:36:46 -0700

    pydocstyle fixups

--- hexbytes/main.py
+++ hexbytes/main.py
@@ -12,22 +12,22 @@ from eth_utils import (
 
 
 class HexBytes(bytes):
-    '''
-    This class is a *very* thin wrapper around the python
+    """
+    HexBytes is a *very* thin wrapper around the python
     built-in :class:`bytes` class. It has these three changes:
 
     1. Accepts hex strings as an initializing value
     2. Returns hex with prefix '0x' from :meth:`HexBytes.hex`
     3. The string representation at console is in hex
-    '''
+    """
     def __new__(cls: Type[bytes], val: Union[bytes, int, str]) -> "HexBytes":
         bytesval = hexstr_if_str(to_bytes, val)
         return cast(HexBytes, super().__new__(cls, bytesval))  # type: ignore  # https://github.com/python/typeshed/issues/2630  # noqa: E501
 
     def hex(self) -> str:
-        '''
+        """
         Just like :meth:`bytes.hex`, but prepends "0x"
-        '''
+        """
         return '0x' + super().hex()
 
     @overload
